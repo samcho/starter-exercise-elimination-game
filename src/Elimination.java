d280cb95214fb17a3c1cd94f452f771207ac3742
Samuel Cho <choss@wfu.edu>
2024-10-03 12:19:16 -0400

    Initial commit

---
+++ src/Elimination.java
@@ -0,0 +1,9 @@
+public class Elimination {
+    // See https://leetcode.com/problems/elimination-game/
+    // Direction: Solve it using an ArrayList
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+}
